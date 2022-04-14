b28edd464855317f022d53bddd354568fd7bf281
Rob Balfre <robert.balfre@travelctm.com.au>
2022-04-14 10:28:28 +1000

    added pnpm + kit + tailwind example

---
+++ kit-tailwind/src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "svelte-select/tailwind.css";
+  import Select from "svelte-select/no-styles/Select.svelte";
+  let items = ['One', 'Two', 'Three'];
+
+</script>
+
+<div class="container mx-auto p-3">
+  <h1 class="text-3xl font-bold mb-2">
+    SvelteKit + tailwind + svelte-select
+  </h1>
+
+  <Select {items} />
+</div>
